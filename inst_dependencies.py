289da859f2ca1af34c8d6c38f6934563feded716
Vladislav <vladislav_semykin01@mail.ru>
2023-11-02 16:54:29 +0300

    Added: File that checks all dependencies and install them if there are none (not existed in the current local machine)

---
+++ inst_dependencies.py
@@ -0,0 +1,25 @@
+import importlib
+
+
+GREEN = "\033[92m"
+RED = "\033[91m"
+BLUE = "\033[94m"
+RESET = "\033[0m"
+
+# List of packages to check and install if not found
+packages_to_check = ["numpy", "torch", "transformers", "sentencepiece", "json"]
+
+for package in packages_to_check:
+    try:
+        importlib.import_module(package)  # Try to import the package
+        print(f"{BLUE}{package}{GREEN} is already installed{RESET}")
+    except ImportError:
+        # If the package is not found, install it
+        print(f"{BLUE}{package}{RESET} is not installed. Installing...")
+        try:
+            # Use pip to install the package
+            import subprocess
+            subprocess.check_call(["pip", "install", package])
+            print(f"{BLUE}{package}{GREEN} has been successfully installed{RESET}")
+        except Exception as excptn:
+            print(f"{RED}Error installing {package}: {str(excptn)}{RESET}")
